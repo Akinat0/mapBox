23e0fd4e2c7db6896558fe6ab187fe33865f9772
Амир Абу махади <abu.amir@mail.ru>
2019-02-08 10:43:52 +0300

    Init commit

---
+++ Assets/Development/Scripts/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("MainCamera").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.LookAt(target);
+	}
+}
---
+++ Assets/Development/Scripts/GeneratePrefab.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePrefab : MonoBehaviour {
+
+    public GameObject prefab;
+
+    private GameObject go;
+
+	// Use this for initialization
+	void Start () {
+        go = Instantiate(prefab);
+        go.transform.position = transform.position;
+        go.transform.localScale = new Vector3(10, 10, 10);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate(){
+        go.transform.position = transform.position;
+    }
+}
